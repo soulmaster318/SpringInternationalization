75676a8aa89de5a515da89b515cd5d380a5f7fc6
soulmaster318 <44565899+soulmaster318@users.noreply.github.com>
2019-11-18 16:32:51 +0700

    Add files via upload

---
+++ SpringBootInternationalization/src/main/java/hiepnguyen/sprint/SpringBootInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package hiepnguyen.sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInternationalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInternationalizationApplication.class, args);
+	}
+
+}
